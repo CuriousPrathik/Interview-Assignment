ce677971ad948ef49df307db64d1f77dc643dc21
Prathik Vishavadiya <108474251+CuriousPrathik@users.noreply.github.com>
2023-06-06 23:19:00 +0530

    Add files via upload

--- Question 2.py
+++ Question 2.py
@@ -1,77 +1,60 @@
-"""
-Question 2: -
+'''
+Q2. A chemist had two chemical flasks labeled 0 and 1 which consist of two
+different chemicals. He extracted 3 features from these chemicals in order to
+distinguish between them, you provided the results derived by the chemicals and
+your task is to create a model that will label chemical 0 or 1 given its three features
+and built-in docker and use some library to display that in frontend.
+Note : Use only pyspark
 
-Consider a string to be valid if all characters of the string appear the same number of times. It is also valid if he can remove just one character at the index in the string, and the remaining characters will occur the same number of times. Given a string, determine if it is valid. If so, return YES , otherwise return NO .
-
-Note - You have to write at least 2 additional test cases in which your program will run successfully and provide an explanation for the same.
-
-Example input 1 - s = “abc”. This is a valid string because frequencies are { “a”: 1, “b”: 1, “c”: 1 }
-
-Example output 1- YES
-
-Example input 2 - s “abcc”. This string is not valid as we can remove only 1 occurrence of “c”. That leaves character frequencies of { “a”: 1, “b”: 1 , “c”: 2 }
-
-Example output 2 - NO
-"""
-
+Dataset:- https://www.kaggle.com/datasets/uciml/indian-liver-patient-records
+'''
 #Ans:
-def is_valid(s):
-    freq = {}
-    for char in s:
-        if char in freq:
-            freq[char] += 1
-        else:
-            freq[char] = 1
-    values = list(freq.values())
-    values.sort()
-    unique_values = set(values)
-    
-    if len(unique_values) == 1:
-        return "YES"
-    elif len(unique_values) == 2:
-        if values.count(values[0]) == 1 and values[0] == 1:
-            return "YES"
-        elif values.count(values[-1]) == 1 and values[-2] == values[-1] - 1:
-            return "NO"  
-    
-    return "NO"
-
-# Example input 1
-
-s = "abc"
-print(is_valid(s))  
-
-# Example output 1 - YES
-
-# Explaination: This is a valid string because frequencies are { “a”: 1, “b”: 1, “c”: 1 }
-
-# Example input 2
-
-s = "abcc"
-print(is_valid(s))  
-
-# Example output 2 - NO
-
-# Explaination: This string is not valid as we can remove only 1 occurrence of “c”. That leaves character frequencies of { “a”: 1, “b”: 1 , “c”: 2 }
-
-# Additional Test Cases
-
-# input 3
-s = "aabbbcc"
-print(is_valid(s))  
-
-# Output: NO
-
-# Explaination: In this case, the string "aabbbcc" has three distinct characters: 'a', 'b', and 'c'. The frequencies of these characters are [2, 3, 2], which are not all equal and cannot be rearranged to form a valid string. No matter how we rearrange the characters, we cannot make the frequencies equal. Therefore, the output is "NO".
-
-# input 4
-
-s = "aabbcc"
-print(is_valid(s))  
-
-# Output: YES
-
-# Explaination: In this case, the string "aabbcc" has three distinct characters: 'a', 'b', and 'c'. The frequencies of these characters are [2, 2, 2], which are all equal. Therefore, the output is "YES" since the string is already valid.
 
 
+# Import necessary libraries
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.sql.functions import col
+
+# Create a Spark session
+spark = SparkSession.builder.appName("ChemicalClassification").getOrCreate()
+
+# Load the dataset
+data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("path_to_dataset.csv")
+
+# Print the schema of the dataset
+data.printSchema()
+
+# Select relevant columns for training
+selected_columns = ['feature1', 'feature2', 'feature3', 'label']
+data = data.select(*selected_columns)
+
+# Convert the label column to numeric type
+data = data.withColumn("label", col("label").cast("double"))
+
+# Split the dataset into training and testing sets
+(training_data, testing_data) = data.randomSplit([0.8, 0.2], seed=42)
+
+# Create a vector assembler to combine the features into a single vector column
+assembler = VectorAssembler(inputCols=['feature1', 'feature2', 'feature3'], outputCol="features")
+
+# Create a logistic regression classifier
+lr = LogisticRegression(labelCol="label", featuresCol="features")
+
+# Create a pipeline to assemble the features and train the model
+pipeline = Pipeline(stages=[assembler, lr])
+
+# Train the model
+model = pipeline.fit(training_data)
+
+# Make predictions on the testing data
+predictions = model.transform(testing_data)
+
+# Display the predictions
+predictions.select("features", "label", "prediction").show()
+
+# Stop the Spark session
+spark.stop()
 
